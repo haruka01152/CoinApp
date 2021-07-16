f35ed2f7373ca42dfa08a5087cd928d21b06e0e9
haruka01152 <haruka.0115maple@yahoo.ne.jp>
2021-07-16 17:56:27 +0900

    テーブルを追加

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,12 @@
 
 @section('content')
 
-
+<table class="table">
+    <tr>
+        <td><i class="fas fa-adjust fa-2x"></i></td>
+        <td>Coin</td>
+        <td>300</td>
+    </tr>
+</table>
 
 @endsection
